fb4fbc577c23049211b0668a3f7f6e99a9eed7e2
HaHyunkyung <87869664+HaHyunkyung@users.noreply.github.com>
2023-11-28 20:24:58 +0900

    fix: 이전 키패드가 0일때 키패드 잘못 반환되는 오류 고침

--- Kakao/키패드_누르기.java
+++ Kakao/키패드_누르기.java
@@ -2,8 +2,8 @@ package Kakao;
 
 public class 키패드_누르기 {
     public static void main(String[] args) {
-        int[] numbers = {1, 3, 4, 5, 8, 2, 1, 4, 5, 9, 5};
-        String hand = "right";
+        int[] numbers = {7, 0, 8, 2, 8, 3, 1, 5, 7, 6, 2};
+        String hand = "left";
         System.out.println(push(numbers,hand));
 
     }
@@ -27,10 +27,10 @@ public class 키패드_누르기 {
                     pad = 11;
                 }
                 String where = check_hand(left,right,pad,hand);
-                if(where == "L"){
+                if(where.equals("L")){
                     left = pad;
                 }
-                if(where == "R"){
+                if(where.equals("R")){
                     right = pad;
                 }
                 answer += where;
@@ -56,7 +56,7 @@ public class 키패드_누르기 {
             if(hand.equals("right")){
                 answer = "R";
             }
-            if(hand.equals("Left")){
+            if(hand.equals("left")){
                 answer = "L";
             }
         }
