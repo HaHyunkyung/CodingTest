7e85313a67d1e456826a9e955ef01dcde604b810
HaHyunkyung <87869664+HaHyunkyung@users.noreply.github.com>
2024-03-08 20:59:57 +0900

    feat: 회문

---
+++ java_study/회문.java
@@ -0,0 +1,30 @@
+package java_study;
+import java.util.Scanner;
+public class 회문 {
+        public static void main(String[] args) {
+            Scanner scanner = new Scanner(System.in);
+
+            System.out.print("input number: ");
+            int inputnum = scanner.nextInt();
+
+            if (isPalindrome(inputnum)) {
+                System.out.println(inputnum + " is palindrome");
+            } else {
+                System.out.println(inputnum + " is not palindrome");
+            }
+        }
+
+        public static int reverse(int number) {
+            int reverse = 0;
+            while (number != 0) {
+                int digit = number % 10;
+                reverse = reverse * 10 + digit;
+                number /= 10;
+            }
+            return reverse;
+        }
+        public static boolean isPalindrome(int number) {
+            return number == reverse(number);
+        }
+
+}
